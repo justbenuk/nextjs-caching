8ddf2fa656bffa87c94ab3525f33dfd89b0c6e0c
justbenuk <justbenuk@gmail.com>
2024-06-04 05:36:30 +0100

    initial commit

--- src/app/messages/page.js
+++ src/app/messages/page.js
@@ -1,11 +1,15 @@
 import Messages from '@/components/messages';
+import { unstable_noStore } from 'next/cache'
+
+//filewide caching
+//export const revalidate = 5
+//export const dynamic = 'force-dynamic'
+
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
-    },
-  });
+  // functionwid cache, this below would be the new common way
+  //unstable_noStore()
+  const response = await fetch('http://localhost:8080/messages');
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
